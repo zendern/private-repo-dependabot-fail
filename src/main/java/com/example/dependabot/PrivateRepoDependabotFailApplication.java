0fe3c2743fcc5cc11b112e8da2b81f5e4da331ae
Nathan Zender <nathan.zender@cardinalhealth.com>
2021-03-04 21:10:54 -0500

    Initial commit of project from spring initializer

---
+++ src/main/java/com/example/dependabot/PrivateRepoDependabotFailApplication.java
@@ -0,0 +1,13 @@
+package com.example.dependabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateRepoDependabotFailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateRepoDependabotFailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dependabot/PrivateRepoDependabotFailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateRepoDependabotFailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
