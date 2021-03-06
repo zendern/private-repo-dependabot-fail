1ef402eba8bca59c190bda8a2f1b26d934911abc
Nathan Zender <github@nathanzender.com>
2021-03-06 14:00:22 -0500

    Adding maven project to match that of the gradle version

---
+++ maven-example/src/main/java/com/example/mavenprivateonly/MavenPrivateOnlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mavenprivateonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenPrivateOnlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenPrivateOnlyApplication.class, args);
+	}
+
+}
---
+++ maven-example/src/test/java/com/example/mavenprivateonly/MavenPrivateOnlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mavenprivateonly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPrivateOnlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
